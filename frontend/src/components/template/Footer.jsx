88fb0cd89c1f3700aee274478c35e0be83dbc673
JuarezFranca <kjootaa@gmail.com>
2021-01-24 17:42:45 -0300

    Routes implement

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -4,7 +4,6 @@ import './Footer.css';
 export default props =>
     <footer className="footer">
         <span>
-            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
-        <strong>Cod<span className="text-danger">3</span>r & Juarez França</strong>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Cod<span className="text-danger">3</span>r & Juarez França</strong>
         </span>
     </footer>
